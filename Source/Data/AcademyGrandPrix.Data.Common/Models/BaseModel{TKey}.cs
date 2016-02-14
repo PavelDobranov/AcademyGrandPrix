88ca63e07cc60996a955068d248ff740dd987807
Pavel Dobranov <pdobranov@gmail.com>
2016-02-15 00:48:06 +0200

    DbRepository implemented

---
+++ Source/Data/AcademyGrandPrix.Data.Common/Models/BaseModel{TKey}.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyGrandPrix.Data.Common.Models
+{
+    class BaseModel_TKey_
+    {
+    }
+}
