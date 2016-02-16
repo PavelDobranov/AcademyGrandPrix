5059001e4ce4f0cf707abcbf16be7657c4f5d908
Pavel Dobranov <pdobranov@gmail.com>
2016-02-16 20:23:42 +0200

    BaseModel{TKey} implemented

--- Source/Data/AcademyGrandPrix.Data.Common/Models/BaseModel{TKey}.cs
+++ Source/Data/AcademyGrandPrix.Data.Common/Models/BaseModel{TKey}.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AcademyGrandPrix.Data.Common.Models
+﻿namespace AcademyGrandPrix.Data.Common.Models
 {
-    class BaseModel_TKey_
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public abstract class BaseModel<TKey> : IAuditInfo, IDeletableEntity
     {
+        [Key]
+        public TKey Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
     }
 }
