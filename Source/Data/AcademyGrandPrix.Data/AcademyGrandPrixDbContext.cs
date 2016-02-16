d3d49182614fbe937775a07827e4b48b42a0c060
Pavel Dobranov <pdobranov@gmail.com>
2016-02-16 20:29:11 +0200

    Added ApplyAuditInfoRules() method in AcademyGrandPrixDbContext class

--- Source/Data/AcademyGrandPrix.Data/AcademyGrandPrixDbContext.cs
+++ Source/Data/AcademyGrandPrix.Data/AcademyGrandPrixDbContext.cs
@@ -1,9 +1,14 @@
 ﻿namespace AcademyGrandPrix.Data
 {
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
     using Microsoft.AspNet.Identity.EntityFramework;
 
     using AcademyGrandPrix.Data.Models;
-    
+    using AcademyGrandPrix.Data.Common.Models;
+
     public class AcademyGrandPrixDbContext : IdentityDbContext<User>
     {
         public AcademyGrandPrixDbContext()
@@ -14,6 +19,33 @@
         public static AcademyGrandPrixDbContext Create()
         {
             return new AcademyGrandPrixDbContext();
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
