7d6667f67f649c36b1b60eb37b80a42204b9f6bf
Pavel Dobranov <pdobranov@gmail.com>
2016-02-20 18:54:29 +0200

    Created TracksController

---
+++ Source/Web/AcademyGrandPrix.Web/Controllers/TracksController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+using AcademyGrandPrix.Services.Data.Contracts;
+using AcademyGrandPrix.Web.ViewModels.Tracks;
+using AcademyGrandPrix.Web.Infrastructure.Mappings;
+
+namespace AcademyGrandPrix.Web.Controllers
+{
+    public class TracksController : BaseController
+    {
+        private readonly ITracksService tracks;
+
+        public TracksController(ITracksService tracks)
+        {
+            this.tracks = tracks;
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            var tracks = this.tracks.GetAll().To<TrackViewModel>();
+
+            var viewModel = new PageableTracksListViewModel
+            {
+                CurrentPage = 1,
+                TotalPages = 1,
+                Tracks = tracks
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
