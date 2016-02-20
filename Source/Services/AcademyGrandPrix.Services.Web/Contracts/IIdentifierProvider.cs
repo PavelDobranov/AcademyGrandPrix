90bf8ea92c446bc0b7a84389b55baaa616101ed9
Pavel Dobranov <pdobranov@gmail.com>
2016-02-20 18:51:09 +0200

    Created new project AcademyGrandPrix.Services.Web with HttpCacheService and IdentifierProvider

---
+++ Source/Services/AcademyGrandPrix.Services.Web/Contracts/IIdentifierProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace AcademyGrandPrix.Services.Web.Contracts
+{
+    public interface IIdentifierProvider
+    {
+        int DecodeId(string urlId);
+
+        string EncodeId(int id);
+    }
+}
