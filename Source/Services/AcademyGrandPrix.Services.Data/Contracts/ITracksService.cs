5f20935d29e7e591d1b316a609e4ce19a892acde
Pavel Dobranov <pdobranov@gmail.com>
2016-02-20 16:03:25 +0200

    Added Tracks service

---
+++ Source/Services/AcademyGrandPrix.Services.Data/Contracts/ITracksService.cs
@@ -0,0 +1,15 @@
+﻿namespace AcademyGrandPrix.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using AcademyGrandPrix.Data.Models;
+
+    public interface ITracksService
+    {
+        IQueryable<Track> GetAll();
+
+        Track GetById(int id);
+
+        IQueryable<Track> GetByDifficulty(TrackDifficultyType difficulty);
+    }
+}
---
+++ Source/Services/AcademyGrandPrix.Services.Data/TracksService.cs
@@ -0,0 +1,33 @@
+﻿namespace AcademyGrandPrix.Services.Data
+{
+    using System.Linq;
+
+    using AcademyGrandPrix.Data.Common;
+    using AcademyGrandPrix.Data.Models;
+    using AcademyGrandPrix.Services.Data.Contracts;
+
+    public class TracksService : ITracksService
+    {
+        private readonly IDbRepository<Track> tracks;
+
+        public TracksService(IDbRepository<Track> tracks)
+        {
+            this.tracks = tracks;
+        }
+
+        public IQueryable<Track> GetAll()
+        {
+            return this.tracks.All();
+        }
+
+        public Track GetById(int id)
+        {
+            return this.tracks.All().FirstOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Track> GetByDifficulty(TrackDifficultyType difficulty)
+        {
+            return this.tracks.All().Where(x => x.Difficulty == difficulty);
+        }
+    }
+}
