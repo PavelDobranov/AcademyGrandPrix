18f7299625098aae78364eedd83b450aa0e32ad3
Pavel Dobranov <pdobranov@gmail.com>
2016-02-20 19:02:10 +0200

    Added AutofacConfig

---
+++ Source/Web/AcademyGrandPrix.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,72 @@
+﻿namespace AcademyGrandPrix.Web
+{
+    using System.Data.Entity;
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using Autofac;
+    using Autofac.Integration.Mvc;
+
+    using AcademyGrandPrix.Data;
+    using AcademyGrandPrix.Data.Common;
+    using AcademyGrandPrix.Services.Data.Contracts;
+    using AcademyGrandPrix.Services.Web;
+    using AcademyGrandPrix.Services.Web.Contracts;
+    using AcademyGrandPrix.Web.Controllers;
+
+    public static class AutofacConfig
+    {
+        public static void RegisterAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            // Register services
+            RegisterServices(builder);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void RegisterServices(ContainerBuilder builder)
+        {
+            builder.Register(x => new AcademyGrandPrixDbContext())
+                .As<DbContext>()
+                .InstancePerRequest();
+
+            builder.Register(x => new HttpCacheService())
+                .As<ICacheService>()
+                .InstancePerRequest();
+
+            builder.Register(x => new IdentifierProvider())
+                .As<IIdentifierProvider>()
+                .InstancePerRequest();
+
+            var servicesAssembly = Assembly.GetAssembly(typeof(ITracksService));
+            builder.RegisterAssemblyTypes(servicesAssembly).AsImplementedInterfaces();
+
+            builder.RegisterGeneric(typeof(DbRepository<>))
+                .As(typeof(IDbRepository<>))
+                .InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .AssignableTo<BaseController>().PropertiesAutowired();
+        }
+    }
+}
