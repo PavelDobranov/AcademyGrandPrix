ae779bc0fafe569432cc75f29c4e9dda190b9dcb
Pavel Dobranov <pdobranov@gmail.com>
2016-02-20 18:54:52 +0200

    Created BaseController

---
+++ Source/Web/AcademyGrandPrix.Web/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿namespace AcademyGrandPrix.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using AutoMapper;
+
+    using AcademyGrandPrix.Services.Web.Contracts;
+    using AcademyGrandPrix.Web.Infrastructure.Mappings;
+
+    public abstract class BaseController : Controller
+    {
+        public ICacheService Cache { get; set; }
+
+        protected IMapper Mapper
+        {
+            get
+            {
+                return AutoMapperConfig.Configuration.CreateMapper();
+            }
+        }
+    }
+}
