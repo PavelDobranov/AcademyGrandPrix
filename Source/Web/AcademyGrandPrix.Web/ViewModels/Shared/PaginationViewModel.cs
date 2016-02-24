49f00b950e8b7753f814531ed298602edca89f5d
Pavel Dobranov <pdobranov@gmail.com>
2016-02-24 05:23:06 +0200

    Pagination extracted in to partial view

---
+++ Source/Web/AcademyGrandPrix.Web/ViewModels/Shared/PaginationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademyGrandPrix.Web.ViewModels.Shared
+{
+    public class PaginationViewModel
+    {
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public string Url { get; set; }
+    }
+}
